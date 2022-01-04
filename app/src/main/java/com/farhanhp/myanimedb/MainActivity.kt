8620f287ad417c2e314e27162bfaeecacd66db17
farhanhp <farhan.herdia123@gmail.com>
2022-01-04 23:25:33 +0700

    fix: change setFilterByAuthorizedAccounts from true to false

--- app/src/main/java/com/farhanhp/myanimedb/MainActivity.kt
+++ app/src/main/java/com/farhanhp/myanimedb/MainActivity.kt
@@ -47,7 +47,7 @@ class MainActivity : AppCompatActivity() {
           // Your server's client ID, not your Android client ID.
           .setServerClientId(getString(R.string.web_client_id))
           // Show all accounts on the device.
-          .setFilterByAuthorizedAccounts(true)
+          .setFilterByAuthorizedAccounts(false)
           .build())
       .build()
   }
